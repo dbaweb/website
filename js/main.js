6413f4ad1dac32e151871134bccab92419109688
Arnaud De Boeck <deboeck.arnaud@gmail.com>
2015-12-08 11:55:36 +0100

    Google map
    Center on Mons

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@
 	var map;
 	function initMap() {
 	  map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: -34.397, lng: 150.644},
+	    center: {lat: 50.455043 , lng: 3.955804},
 	    zoom: 8
 	  });
 	}
