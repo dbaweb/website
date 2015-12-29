997de8f50d6af84fd1b0c3315f0f93fa8e81b37d
Arnaud De Boeck <deboeck.arnaud@gmail.com>
2015-12-29 13:39:52 +0100

    A
    A

--- js/map.js
+++ js/map.js
@@ -1,18 +1,60 @@
+// Google Map
+
+    // Variables
+
 var map;
+
+    // Styles
+
 var styles = [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#ed5929"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#c4c4c4"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#ed5929"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21},{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ed5929"},{"lightness":"0"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#ed5929"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#575757"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.stroke","stylers":[{"color":"#2c2c2c"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#999999"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+
+    // Position
+
+var ma_position = {lat:50.454998, lng: 3.955845 };
+
+    // Init Map
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 50.467524, lng: 3.951681},
+    center: ma_position,
     zoom: 16,
-    minZoom: 14,
-    panControl: false,
-	zoomControl: false,
-    mapTypeControl: false,
-	scaleControl: false,
-	streetViewControl: false,
-	overviewMapControl: false
+    minZoom: 14
   });
-  map.setOptions({styles: styles});
-}
 
+    // Set Styles
+
+  map.setOptions({styles: styles});
+
+    // Info Window
+
+var content_window = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Contact Informations</h1>'+
+      '<div id="bodyContent">'+
+      '<ul>'+
+  '<li>Tél.: 0474785158</li>'+
+  '<li>Mail: deboeck.arnaud@gmail.com</li>'+
+  '<li>Adress: Rue de la raquette, 38 </li>'+
+  '<li>7000 MONS</li>'+
+  '</ul>'+
+      '</div>'+
+      '</div>';
+
+    // Create Window
+
+  var infowindow = new google.maps.InfoWindow({
+    content: content_window
+  });
+
+    // Create Marker
+
+  var marker = new google.maps.Marker({
+    position: ma_position,
+    map: map,
+    title: 'Uluru (Ayers Rock)'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
